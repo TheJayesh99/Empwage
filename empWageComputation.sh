c7a4c12e44eff5571ce72f9df04a1c24ee1b38a7
TheJayesh99 <jayeshm.official@gmail.com>
2021-06-15 19:21:09 +0530

    Attendence check

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,18 @@
 #! /bin/bash -x
 
-echo "Welcome to  EmpComputation"
+echo "Welcome to EmpComputation"
+
+#contant
+IS_PRESENT=1
+
+#variable
+EmpCheck=$(( RANDOM % 2))
+
+if [ $EmpCheck -eq $IS_PRESENT ]
+then
+        echo "emp present"
+else
+        echo "emp absent"
+fi
+
+
